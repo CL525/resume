a704ea0eb4d7d0fd3a6829ea0e90097ed1828739
CL525 <cl790869483@163.com>
2024-01-16 13:15:53 +0800

    修改信息

--- src/components/basic-info/data.ts
+++ src/components/basic-info/data.ts
@@ -2,8 +2,8 @@
 export const BasicInfoData = {
   age: { title: '年龄', value: '1993/03/19' },
   period: { title: '工作年限', value: '7年工作经验' },
-  email: { title: '邮箱', value: '790869483@qq.com' },
   mobile: { title: '手机', value: '17621625233' },
+  email: { title: '邮箱', value: '790869483@qq.com' },
   location: { title: '籍贯', value: '安徽安庆' },
-  college: { title: '大学', value: '滁州学院' },
+  college: { title: '大学', value: '滁州学院 2012 - 2016' },
 }
