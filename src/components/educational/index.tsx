49c43290266aa9ddc0e9f76787214182445519b6
CL525 <cl790869483@163.com>
2024-01-20 12:23:33 +0800

    更新ui

--- src/components/educational/index.tsx
+++ src/components/educational/index.tsx
@@ -8,10 +8,10 @@ export default function() {
     <ModuleContainer title="教育经历">
       <div className="educational_container">
         <Icon className="icon" name={'maki:college'} />
-        <span className="educational_container-item">滁州学院</span>
-        <span className="educational_container-item title">本科</span>
-        <span className="educational_container-item title grow">电子信息工程</span>
-        <span className="educational_container-item time">2012.09 - 2016.06</span>
+        <span className="">滁州学院</span>
+        <span className="educational_container-title">本科</span>
+        <span className="educational_container-title grow">电子信息工程</span>
+        <span className="educational_container-time">2012.09 - 2016.06</span>
       </div>
     </ModuleContainer>
   )
